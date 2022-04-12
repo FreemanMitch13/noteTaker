4dd581bd35aeb98944870c0dd22719f04918f75e
Mitchell Freeman <mitchell.w.freeman13@gmail.com>
2022-04-12 05:34:49 -0400

    api routes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,36 @@
 const note = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+note.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+note.post('/', (req, res) => {
+    
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote={
+        title,
+        text,
+        id: uuidv4()
+      };
+      readAndAppend(newNote, './db/db.json'); 
+      res.json('Your Note has been added');
+    } else {
+      res.json('Error');
+    }
+  });
+  
+  note.delete('/:id', (req, res) => {
+      const Id = req.params.id;
+      readFromFile('./db/db.json').then((data) => JSON.parse(data)).then((json) => {
+          const result = json.filter((note) => note.id !==Id);
+        writeToFile('./db/db.json', result);
+        res.json('Your Note has been deleted');
+      });
+  });
+
+
+module.exports = note;
