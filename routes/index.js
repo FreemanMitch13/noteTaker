7149c6918467152b1e945a15927a0271b5c40128
Mitchell Freeman <mitchell.w.freeman13@gmail.com>
2022-04-29 15:39:46 -0400

    testing heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const apiRoutes = require('./apiRoutes');
+// const apiRoutes = require('./apiRoutes');
 const htmlRoutes = require('./htmlRoutes');
 const app = express();
 
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 module.exports = app;
